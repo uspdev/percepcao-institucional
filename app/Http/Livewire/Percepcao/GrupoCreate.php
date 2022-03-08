cd95bb53fc62a07400bd5311d78f14da8d16bdc5
Gustavo Medeiros <gustavome@usp.br>
2022-03-08 12:32:12 +0000

    Cria livewire create e show do grupo

---
+++ app/Http/Livewire/Percepcao/GrupoCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Percepcao;
+
+use Livewire\Component;
+
+class GrupoCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.percepcao.grupo-create');
+    }
+}
---
+++ app/Http/Livewire/Percepcao/GrupoShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Percepcao;
+
+use Livewire\Component;
+
+class GrupoShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.percepcao.grupo-show');
+    }
+}
