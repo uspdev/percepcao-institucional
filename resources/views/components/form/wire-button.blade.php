67ec950a5efd500e28a6670fde22d6700478c19b
Gustavo Medeiros <gustavome@usp.br>
2022-05-03 14:40:34 +0000

    Inclusão deuso de novos ícones como componentes

--- resources/views/components/form/wire-button.blade.php
+++ resources/views/components/form/wire-button.blade.php
@@ -9,14 +9,23 @@
     @endif
 
     {{ $attributes }}>
+    @if ($action === 'question')
+        <x-icon.question class="{{ $classIcon }}" />
+    @endif
     @if ($action === 'preview')
-        <x-icon.preview />
+        <x-icon.preview class="{{ $classIcon }}" />
     @endif
     @if ($action === 'update')
-        <x-icon.update />
+        <x-icon.update class="{{ $classIcon }}" />
     @endif
     @if ($action === 'delete')
-        <x-icon.delete />
+        <x-icon.delete class="{{ $classIcon }}" />
+    @endif
+    @if ($action === 'add')
+        <x-icon.add class="{{ $classIcon }}" />
+    @endif
+    @if ($action === 'save')
+        <x-icon.save class="{{ $classIcon }}" />
     @endif
     {{ $label }}
 </button>
