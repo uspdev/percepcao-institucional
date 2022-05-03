2ae9995571b568d4b6e9d0e52fe6d0ef6f0c4a61
Gustavo Medeiros <gustavome@usp.br>
2022-05-03 14:48:45 +0000

    Alteração para que mensagens de erro possam ser exibidas como html

--- resources/views/components/form/wire-select.blade.php
+++ resources/views/components/form/wire-select.blade.php
@@ -1,42 +1,42 @@
 <div class="form-group {{ $class }}">
-  @if ($label)
-    <label>{!! $label !!}</label><br />
-  @endif
-  <div class="input-group">
-    @if ($prepend)
-      <div class="input-group-prepend">
-        <div class="input-group-text">{{ $prepend }}</div>
-      </div>
+    @if ($label)
+        <label>{!! $label !!}</label><br />
     @endif
-    <select
+    <div class="input-group">
+        @if ($prepend)
+            <div class="input-group-prepend">
+                <div class="input-group-text">{{ $prepend }}</div>
+            </div>
+        @endif
+        <select
 
-      class="form-control"
+            class="form-control"
 
-      wire:model{{ $wireModifier }}="{{ $model }}"
+            wire:model{{ $wireModifier }}="{{ $model }}"
 
-      @if($multiple)
-          multiple
-      @endif
+            @if($multiple)
+                multiple
+            @endif
 
-      {{ $attributes }}
+            {{ $attributes }}
 
-      >
+            >
 
-      @if($placeholder)
-          <option value="" disabled selected hidden>
-              {{ $placeholder }}
-          </option>
-      @endif
+            @if ($placeholder)
+                <option value="" disabled selected hidden>
+                    {{ $placeholder }}
+                </option>
+            @endif
 
-      {{ $slot }}
+            {{ $slot }}
 
-      @foreach ($options as $key => $val)
-        <option value="{{ $key }}">{{ $val }}</option>
-      @endforeach
+            @foreach ($options as $key => $val)
+                <option value="{{ $key }}">{{ $val }}</option>
+            @endforeach
 
-    </select>
-  </div>
-  @error($model)
-    <div class="small text-danger">{{ $message }}</div>
-  @enderror
+        </select>
+    </div>
+    @error($model)
+        <div class="small text-danger">{!! $message !!}</div>
+    @enderror
 </div>
