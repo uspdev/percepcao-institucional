ee718e1cbaa6834b2813b7f370a9ae9b34c9cf32
Gustavo Medeiros <gustavome@usp.br>
2022-05-19 14:18:25 +0000

    Criação do model e migration Disciplina

---
+++ database/migrations/2022_05_13_133626_create_disciplinas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisciplinasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplinas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('codpes');
+            $table->string('nompes');
+            $table->string('coddis');
+            $table->string('nomdis');
+            $table->integer('verdis');
+            $table->string('codtur');
+            $table->string('tiptur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disciplinas');
+    }
+}
