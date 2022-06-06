b9af21c6785ba250b7cde46603f57be8778d06db
Gustavo Medeiros <gustavome@usp.br>
2022-06-06 17:05:29 +0000

    Alterando exibição de mensagens para as do campo settings

--- resources/views/livewire/percepcao/percepcao-avaliacao-show.blade.php
+++ resources/views/livewire/percepcao/percepcao-avaliacao-show.blade.php
@@ -2,13 +2,13 @@
     @if (empty($statusPercepcao))
         <p>&nbsp;</p>
         <pre class="font-weight-bold" style='white-space: pre-wrap; font-size: 16px; text-align: center'>
-            @isset ($percepcao->settings['textoApresentacao'] && !empty(isset($percepcao->settings['textoApresentacao'])))
+            @if (isset($percepcao->settings['textoApresentacao']) && !empty($percepcao->settings['textoApresentacao']))
               {!! $percepcao->settings['textoApresentacao'] !!}
-            @endisset
+            @endif
         </pre>
         <p class="font-weight-bold" style='text-align: center'>
             @if ($percepcaoEnvio)
-                @if (isset($percepcao->settings['textoAgradecimentoEnvioAvaliacao']) && !empty(isset($percepcao->settings['textoAgradecimentoEnvioAvaliacao'])))
+                @if (isset($percepcao->settings['textoAgradecimentoEnvioAvaliacao']) && !empty($percepcao->settings['textoAgradecimentoEnvioAvaliacao']))
                     <span class='text-success'>{!! $percepcao->settings['textoAgradecimentoEnvioAvaliacao'] !!}</span>
                 @else
                     <span class='text-success'>Obrigado pela sua participação no processo de avaliação de disciplinas deste semestre.</span>
