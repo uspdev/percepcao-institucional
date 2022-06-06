a1ff4eea7853d0baa23bfb28f1019ed0db1d5635
Gustavo Medeiros <gustavome@usp.br>
2022-06-06 15:11:00 +0000

    Alterando exibição de mensagens para as do campo settings

--- resources/views/livewire/percepcao/percepcao-avaliacao-show.blade.php
+++ resources/views/livewire/percepcao/percepcao-avaliacao-show.blade.php
@@ -1,25 +1,26 @@
 <div>
-  @if(empty($statusPercepcao))
-    <p>&nbsp;</p>
-    <pre class="font-weight-bold" style='white-space: pre-wrap; font-size: 16px; text-align: center'>
-      No período do dia {{ $percepcao->dataDeAbertura->translatedFormat('d/m/Y \à\s H:i:s \(l\)') }} ao dia {{ $percepcao->dataDeFechamento->translatedFormat('d/m/Y \à\s H:i:s \(l\)') }}, estará aberto o Processo de Avaliação de Disciplinas.
-      A participação de todos os discentes é muito importante e permitirá o levantamento de informações que levarão a um processo contínuo de melhoria da qualidade de ensino na EEL-USP.
-      Não deixem de responder o Questionário preparado especialmente para vocês, alunos. Além de perguntas direcionadas, existem campos de texto que podem ser usados livremente.
-      Todas as questões devem ser respondidas antes de submeter o Questionário, mas não serão necessários mais que 10-15 minutos.
-
-      A Comissão de Avaliação de Disciplinas (CAD).
-    </pre>
-    <p class="font-weight-bold" style='text-align: center'>
-      @if($percepcaoEnvio)
-        <span class='text-success'>Obrigado pela sua participação no processo de avaliação de disciplinas deste semestre.</span>
-      @else
-        <a href='avaliar'>CLIQUE AQUI PARA INICIAR O PROCESSO DE AVALIAÇÃO DE DISCIPLINAS</a>
-      @endif
-    </p>
-  @else
-    <p>&nbsp;</p>
-    <p class="font-weight-bold" style='text-align: center'>
-      {{ $statusPercepcao }}
-    </p>
-  @endif
+    @if (empty($statusPercepcao))
+        <p>&nbsp;</p>
+        <pre class="font-weight-bold" style='white-space: pre-wrap; font-size: 16px; text-align: center'>
+            @isset ($percepcao->settings['textoApresentacao'] && !empty(isset($percepcao->settings['textoApresentacao'])))
+              {!! $percepcao->settings['textoApresentacao'] !!}
+            @endisset
+        </pre>
+        <p class="font-weight-bold" style='text-align: center'>
+            @if ($percepcaoEnvio)
+                @if (isset($percepcao->settings['textoAgradecimentoEnvioAvaliacao']) && !empty(isset($percepcao->settings['textoAgradecimentoEnvioAvaliacao'])))
+                    <span class='text-success'>{!! $percepcao->settings['textoAgradecimentoEnvioAvaliacao'] !!}</span>
+                @else
+                    <span class='text-success'>Obrigado pela sua participação no processo de avaliação de disciplinas deste semestre.</span>
+                @endif
+            @else
+              <a href='avaliar'>CLIQUE AQUI PARA INICIAR O PROCESSO DE AVALIAÇÃO DE DISCIPLINAS</a>
+            @endif
+        </p>
+    @else
+        <p>&nbsp;</p>
+        <p class="font-weight-bold" style='text-align: center'>
+            {{ $statusPercepcao }}
+        </p>
+    @endif
 </div>
