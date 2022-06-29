45e66c540dfc6002170855163063fb913456f8ec
Masaki Kawabata Neto <kawabata@usp.br>
2022-06-29 12:14:08 -0300

    diversos seeders

--- database/seeders/GrupoSeeder.php
+++ database/seeders/GrupoSeeder.php
@@ -15,6 +15,7 @@ class GrupoSeeder extends Seeder
     public function run()
     {
         \App\Models\Grupo::create([
+            'id' => 1,
             'texto' => 'PERCEPÇÃO DO ALUNO EM CADA DISCIPLINA',
             'ativo' => 1,
             'repeticao' => 1,
@@ -23,6 +24,7 @@ class GrupoSeeder extends Seeder
             'updated_at' => now(),
         ]);
         \App\Models\Grupo::create([
+            'id' => 2,
             'texto' => 'AUTO AVALIAÇÃO DO ALUNO NA DISCIPLINA',
             'ativo' => 1,
             'repeticao' => 1,
@@ -32,6 +34,7 @@ class GrupoSeeder extends Seeder
             'updated_at' => now(),
         ]);
         \App\Models\Grupo::create([
+            'id' => 3,
             'texto' => 'COMENTÁRIOS E SUGESTÕES DO ALUNO NA DISCIPLINA',
             'ativo' => 1,
             'repeticao' => 1,
@@ -41,22 +44,65 @@ class GrupoSeeder extends Seeder
             'updated_at' => now(),
         ]);
         \App\Models\Grupo::create([
+            'id' => 4,
             'texto' => 'COMENTÁRIOS E SUGESTÕES GERAIS',
             'ativo' => 1,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         \App\Models\Grupo::create([
+            'id' => 5,
             'texto' => 'Infraestrutura, condições de ensino e da instituição',
             'ativo' => 1,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         \App\Models\Grupo::create([
+            'id' => 6,
             'texto' => 'PERCEPÇÃO DA COORDENAÇÃO',
             'ativo' => 1,
             'repeticao' => 1,
             'modelo_repeticao' => 'coordenadores',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        \App\Models\Grupo::create([
+            'id' => 7,
+            'texto' => 'DISCIPLINA',
+            'ativo' => 1,
+            'repeticao' => 1,
+            'modelo_repeticao' => 'disciplinas',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        \App\Models\Grupo::create([
+            'id' => 8,
+            'texto' => 'Avaliação do professor',
+            'ativo' => 1,
+            'repeticao' => 1,
+            'modelo_repeticao' => 'disciplinas',
+            'parent_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        \App\Models\Grupo::create([
+            'id' => 9,
+            'texto' => 'Avaliação da disciplina',
+            'ativo' => 1,
+            'repeticao' => 1,
+            'modelo_repeticao' => 'disciplinas',
+            'parent_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        \App\Models\Grupo::create([
+            'id' => 10,
+            'texto' => 'Auto avaliação',
+            'ativo' => 1,
+            'repeticao' => 1,
+            'modelo_repeticao' => 'disciplinas',
+            'parent_id' => 7,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
--- database/seeders/QuestaoSeeder.php
+++ database/seeders/QuestaoSeeder.php
@@ -21,7 +21,18 @@ class QuestaoSeeder extends Seeder
             ["key" => "5", "value" => "Muito bom"],
         ];
 
+        $optionsSImNao = [
+            ["key" => "1", "value" => "Sim"],
+            ["key" => "2", "value" => "Não"],
+        ];
+
+        $optionsCumprimento = [
+            ["key" => "1", "value" => "Não cumprido"],
+            ["key" => "2", "value" => "Cumprido integralmente"],
+        ];
+
         $textos = [
+            // EEL
             'Assiduidade/Pontualidade',
             'Apresentação/Cumprimento do plano de ensino',
             'Conhecimento/Atualização da matéria',
@@ -32,6 +43,11 @@ class QuestaoSeeder extends Seeder
             'Coerência entre conteúdo ministrado e avaliações aplicadas',
             'Interesse em esclarecer as dúvidas dos alunos',
             'Relacionamento com os alunos',
+
+            'Compromisso',
+            'Participação nas aulas',
+            'Relacionamento com os colegas, professores e pessoal administrativo',
+            'Cumprimento das atividades recomendadas',
 
             'Iluminação, circulação de ar e acústica das salas de aula',
             'Espaço físico/número de alunos das salas de aula',
@@ -58,6 +74,122 @@ class QuestaoSeeder extends Seeder
                 'ativo' => 1,
             ]);
         }
+
+        $campos = [
+            [
+                'type' => 'radio',
+                'text' => 'Como você avalia a pontualidade e assiduidade do professor?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'O professor apresentou um plano de ensino?',
+                'options' => [["key" => "1", "value" => "Sim"], ["key" => "2", "value" => "Não"]],
+                'rules' => 'required|integer|min:1|max:2',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'O plano de ensino foi cumprido?',
+                'options' => [["key" => "1", "value" => "Sim"], ["key" => "2", "value" => "Não"]],
+                'rules' => 'required|integer|min:1|max:2',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia o domínio do professor na disciplina?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Os recursos disponibilizados (PDF\'s, vídeo aulas, listas de exercícios, exemplos em aula, vídeos, imagens, áudios, softwares, etc) são suficientes e adequados para o aprendizado e a realização das avaliações?',
+                'options' => $options, // precisa ajustar
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia a didática (dinamismo, clareza e organização na exposição de temas) nas aulas ministradas?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como você avalia a coerência entre conteúdo ministrado e avaliações aplicadas?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia a abertura e disponibilidade do(a) docente a respeito de dúvidas de conteúdo e da programação da disciplina?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia a preocupação do(a) docente com o processo de aprendizado do conteúdo?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+
+            [
+                'type' => 'radio',
+                'text' => 'Quão atualizada é o conteúdo da  disciplina?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'A bibliografia básica contempla o conteúdo principal abordado na disciplina?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como você avalia a conexão entre aspectos teóricos e práticos da disciplina?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Você  tem entendimento do papel formativo da disciplina na estrutura do seu curso?',
+                'options' => [["key" => "1", "value" => "Pouco"], ["key" => "2", "value" => "Parcial"], ["key" => "3", "value" => "Muito"]],
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+
+            // aluno na disciplina
+            [
+                'type' => 'radio',
+                'text' => 'Como você avalia a sua pontualidade e assiduidade? ',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia a sua participação nas aulas (perguntar, realizar as atividades propostas durante a aula)?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Quão bem você cumpriu as atividades recomendadas?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+            [
+                'type' => 'radio',
+                'text' => 'Como avalia a sua colaboração com colegas no desenvolvimento de atividades propostas durante as aulas (empatia com os colegas)?',
+                'options' => $options,
+                'rules' => 'required|integer|min:1|max:5',
+            ],
+        ];
+
+        foreach ($campos as $campo) {
+            \App\Models\Questao::create([
+                'campo' => $campo,
+                'ativo' => 1,
+            ]);
+        }
+
         \App\Models\Questao::create([
             'campo' => [
                 'type' => 'textarea',
@@ -69,6 +201,7 @@ class QuestaoSeeder extends Seeder
             ],
             'ativo' => 1,
         ]);
+
         \App\Models\Questao::create([
             'campo' => [
                 'type' => 'textarea',
