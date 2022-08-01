cbe2b40520010ed5fe890ae03f836de47ec6d4d6
Masaki Kawabata Neto <kawabata@usp.br>
2022-08-01 15:16:35 -0300

    mostrando ações

---
+++ resources/views/percepcao/partials/show-secundario.blade.php
@@ -0,0 +1,6 @@
+<b>Ações</b>
+<div class="ml-2">
+    Liberar membros especiais<br>
+    Liberar docentes<br>
+    Liberar alunos
+</div>
--- resources/views/percepcao/show.blade.php
+++ resources/views/percepcao/show.blade.php
@@ -13,38 +13,14 @@
 
   <div class="card">
     <div class="card-body">
-      <p>
-        Período: de <b>{{ $percepcao->dataDeAbertura->format('d/m/Y H:i') }}</b> à
-        <b>{{ $percepcao->dataDeFechamento->format('d/m/Y H:i') }}</b>
-      </p>
-      <p>
-        <a href="{{ route('avaliar.preview', $percepcao->id) }}">
-          <i class="fas fa-eye"></i>
-          Visualização prévia do questionário
-        </a>
-      </p>
-      <p>
-        <a href="{{ route('percepcao.questoes', $percepcao->id) }}">
-          <i class="fas fa-question-circle"></i>
-          Adicionar/editar questões
-        </a>
-      </p>
-      <p>
-        Alunos matriculados: {{ $percepcao->settings['totalDeAlunosMatriculados'] }}
-        <i class="fas fa-angle-right"></i>
-        <a href="{{ route('percepcao.alunos', $percepcao->id) }}">
-          <i class="fas fa-eye"></i>
-          ver lista
-        </a>
-      </p>
-      <p>
-        Turmas: {{ $percepcao->settings['totalDeDisciplinas'] }}
-        <i class="fas fa-angle-right"></i>
-        <a href="{{ route('percepcao.disciplinas', $percepcao->id) }}">
-          <i class="fas fa-eye"></i>
-          ver lista
-        </a>
-      </p>
+      <div class="row">
+        <div class="col-md-7">
+          @include('percepcao.partials.show-principal')
+        </div>
+        <div class="col-md-5">
+          @include('percepcao.partials.show-secundario')
+        </div>
+      </div>
     </div>
   </div>
 
