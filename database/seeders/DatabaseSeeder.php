aebb0bafee27d3efc8f997fac1ef9f27d7b42909
Gustavo Medeiros <gustavome@usp.br>
2022-08-16 15:55:37 +0000

    Adição de chamada para o seeder da Disciplinas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             GrupoSeeder::class,
             QuestaoSeeder::class,
             PercepcaoSeeder::class,
+            DisciplinaSeeder::class,
             RespostaSeeder::class,
         ]);
     }
